d99c55371ed0fbc20a177770452a98c6e45f22aa
Michael Cruz <44488454+UBERCRUZER@users.noreply.github.com>
2020-04-20 20:43:38 -0700

    Delete Settings.py

--- Settings.py
+++
@@ -1,6 +0,0 @@
-
-HOST = "irc.twitch.tv"
-PORT = 6667
-PASS = "oauth:nb2r1ivp29issh5vjzecq1lvspgf73" # your Twitch OAuth token
-IDENT = "monstersBRUHbot"  # Twitch username your using for your bot
-CHANNEL = "martinimonsters"   # Channel
